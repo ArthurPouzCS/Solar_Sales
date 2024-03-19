1844a2a1d253009b663db631d571d4dedd3e474e
ArthurPouzCS <arthur.pouzargue@student-cs.fr>
2024-03-19 15:10:32 +0100

    pdf14

--- generation_pdf/audit_html.py
+++ generation_pdf/audit_html.py
@@ -132,7 +132,7 @@ def audit_string_html(dic):
                     max-height: 200px;
                     float:right;
                     margin-right: 20%;
-                    margin-top: 550px;
+                    margin-top: 400px;
                 }
 
                 div span {
